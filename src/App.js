312fb2ac1c6fc02d66542e20b5aa0f3b1e725caa
Gunjan Malviya <GMalviya@bpl.netlink.com>
2024-03-27 14:01:17 +0530

    Adding sideMenu

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import "./App.css";
+import SideMenu from "./components/SideMenu";
+
+
 
 function App() {
-  return <div className="App">Prashant</div>;
-}
+  return (
+    <>
+<SideMenu/>
+  </>
+  )
+};
 
 export default App;
